30dff5903ac0815eb8eaf39d875a7b4441d2d9db
Calloop <CalloopG@gmail.com>
2021-03-05 01:58:58 +0300

    added sign in activity

--- app/src/main/java/ru/startandroid/develop/pikabu_demo/MainActivity.java
+++ app/src/main/java/ru/startandroid/develop/pikabu_demo/MainActivity.java
@@ -2,13 +2,29 @@ package ru.startandroid.develop.pikabu_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSignIn = findViewById(R.id.buttonSignIn);
+        buttonSignIn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.buttonSignIn) {
+            Intent intent = new Intent(this, SignInActivity.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/ru/startandroid/develop/pikabu_demo/SignInActivity.java
@@ -0,0 +1,14 @@
+package ru.startandroid.develop.pikabu_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
