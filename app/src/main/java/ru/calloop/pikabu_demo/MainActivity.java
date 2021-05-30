f4f0dbf5ba0e2a0810555f2c13ebe6fbc3259b77
Calloop <CalloopG@gmail.com>
2021-05-30 20:53:24 +0300

    30.05.21

--- app/src/main/java/ru/calloop/pikabu_demo/MainActivity.java
+++ app/src/main/java/ru/calloop/pikabu_demo/MainActivity.java
@@ -1,29 +1,118 @@
 package ru.calloop.pikabu_demo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import ru.calloop.pikabu_demo.adapter.SessionManager;
+import ru.calloop.pikabu_demo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity{
 
     Toolbar toolbarMain;
+    FloatingActionButton fabCreatePost;
     Button buttonSignIn;
     ImageButton imageButtonCreatePost;
     SessionManager sessionManager;
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbarMain = findViewById(R.id.toolbar_main);
+        fabCreatePost = findViewById(R.id.fabCreatePost);
+        fabCreatePost.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CreatePostActivity.class);
+            startActivity(intent);
+        });
+
+//        toolbarMain = findViewById(R.id.toolbarMain);
+//        setSupportActionBar(toolbarMain);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         sessionManager = new SessionManager(getApplicationContext());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar_main_activity, menu);
+        return true;
+    }
+
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarDrawerMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_drawer_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawer_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_drawer_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //        if (sessionManager.getLogin() == true)
 //        {
@@ -45,5 +134,4 @@ public class MainActivity extends AppCompatActivity{
 //            Intent intent = new Intent(MainActivity.this, CreatePostActivity.class);
 //            startActivity(intent);
 //        });
-    }
 }
